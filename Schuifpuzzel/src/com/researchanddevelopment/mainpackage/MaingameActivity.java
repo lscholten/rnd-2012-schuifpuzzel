2b5325564b0db58ee374fa00c4728d8074f79200
thom@thomwiggers.nl <thom@thomwiggers.nl@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-19 10:41:19 +0000

    2.1

--- Schuifpuzzel/src/com/researchanddevelopment/mainpackage/MaingameActivity.java
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/MaingameActivity.java
@@ -13,7 +13,7 @@ public class MaingameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.schuifpuzzel);
+		setContentView(R.layout.main);
 	}
 
 }
