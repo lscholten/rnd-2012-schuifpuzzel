789ca5cfb1ec6271e2a666d1078bf23f925d5844
thom@thomwiggers.nl <thom@thomwiggers.nl@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-19 10:32:29 +0000

    lalalala arm

--- Schuifpuzzel/src/com/researchanddevelopment/mainpackage/SchuifpuzzelActivity.java
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/SchuifpuzzelActivity.java
@@ -6,19 +6,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SchuifpuzzelActivity extends Activity {
-	
+
 	private Button b;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		b = (Button)findViewById(R.id.startGame);
-		b.setOnClickListener(new SchuifpuzzelActivityListener(this));
+	}
+
+	/**
+	 * past titel aan
+	 * 
+	 * @param v
+	 */
+	public void AboutLaunch(View v) {
+		((Button) v).setText("Porno is op");
+		TextView t = (TextView) findViewById(R.id.titel1);
+		t.setText("Porno");
 	}
 
 	@Override
@@ -29,13 +40,14 @@ public class SchuifpuzzelActivity extends Activity {
 		mi.inflate(R.menu.menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 		case R.id.about:
-			Intent i = new Intent("com.researchanddevelopment.mainpackage.ABOUT");
+			Intent i = new Intent(
+					"com.researchanddevelopment.mainpackage.ABOUT");
 			startActivity(i);
 			break;
 		case R.id.exit:
@@ -44,8 +56,5 @@ public class SchuifpuzzelActivity extends Activity {
 		}
 		return false;
 	}
-	
-	
 
-	
 }
