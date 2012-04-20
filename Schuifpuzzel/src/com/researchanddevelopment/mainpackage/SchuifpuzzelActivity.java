5dbb287a441c1f4b1afb01723c2f44dee0cc0619
thom@thomwiggers.nl <thom@thomwiggers.nl@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-20 12:45:40 +0000

    more cleaning

--- Schuifpuzzel/src/com/researchanddevelopment/mainpackage/SchuifpuzzelActivity.java
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/SchuifpuzzelActivity.java
@@ -12,8 +12,6 @@ import android.widget.TextView;
 
 public class SchuifpuzzelActivity extends Activity {
 
-	private Button b;
-
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
