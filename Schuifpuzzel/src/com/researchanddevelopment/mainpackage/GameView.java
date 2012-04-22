6e9c96b92b377415a71d28aed99187660e4f52c7
CoolguyNL@gmail.com <CoolguyNL@gmail.com@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-22 10:26:12 +0000

    drawCar

--- Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameView.java
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameView.java
@@ -1,14 +1,19 @@
 package com.researchanddevelopment.mainpackage;
 
+import com.researchanddevelopment.mainpackage.Auto.Orientation;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GameView extends View {
 	private Paint p;
+	Auto auto;
+	Auto auto2;
 	
 	private static final int TILE_SIZE = 65;
 	
@@ -16,6 +21,8 @@ public class GameView extends View {
 		super(context);
 		// TODO Auto-generated constructor stub
 		p = new Paint();
+		auto = new Auto(new Point(0,0), 3, Auto.Orientation.HORIZONTAAL);
+		auto2 = new Auto(new Point(4,4), 2, Auto.Orientation.VERTICAAL);
 		
 	}
 
@@ -32,8 +39,23 @@ public class GameView extends View {
 				canvas.drawRect(5+TILE_SIZE*i, 5+TILE_SIZE*j, TILE_SIZE*(i+1) , TILE_SIZE*(j+1), p);
 			}
 		}
-		
+		canvas = drawCar(this.auto, canvas);
+		canvas = drawCar(this.auto2, canvas);
 		super.onDraw(canvas);
+	}
+	
+	private Canvas drawCar(Auto a, Canvas c){
+		p.setColor(Color.RED);
+		int xLeft = a.getPos().x;
+		int yTop = a.getPos().y;
+		int length = a.getLength();
+		
+		if(a.getOrientation() == Orientation.HORIZONTAAL )
+			c.drawRect(5 + TILE_SIZE * xLeft, 5 + TILE_SIZE * yTop , TILE_SIZE * (xLeft + length), TILE_SIZE * (yTop + 1), p);
+		else
+			c.drawRect(5 + TILE_SIZE * xLeft, 5 + TILE_SIZE * yTop, TILE_SIZE * (xLeft + 1), TILE_SIZE * (yTop + length), p);
+		
+		return c;
 	}
 	
 	
