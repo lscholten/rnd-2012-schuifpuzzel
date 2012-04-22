1ee7768ba2537645ccd873dbc1c8e49c52117bc0
CoolguyNL@gmail.com <CoolguyNL@gmail.com@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-22 21:04:13 +0000

    Autolistener

--- trunk/Schuifpuzzel/src/com/researchanddevelopment/mainpackage/AutoListener.java
+++ trunk/Schuifpuzzel/src/com/researchanddevelopment/mainpackage/AutoListener.java
@@ -1,19 +1,39 @@
 package com.researchanddevelopment.mainpackage;
 
+import java.util.ArrayList;
+
+import com.researchanddevelopment.mainpackage.Auto.Orientation;
+
+import android.graphics.PointF;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
 public class AutoListener implements OnTouchListener{
 
-	Auto auto;
+	ArrayList<Auto> auto;
 	
-	public AutoListener(Auto a){
+	public AutoListener(ArrayList<Auto> a){
 		this.auto = a;
 	}
 	
 	public boolean onTouch(View v, MotionEvent event) {
 		// TODO Auto-generated method stub
+		Auto activecar = null;
+		System.out.println(v.toString());
+		for(Auto auto : this.auto){
+			if(auto.touchOnPosition(event.getX(), event.getY(), BoardView.TILE_SIZE)){
+				activecar = auto;
+				break;
+			}
+		}
+		if(activecar != null){
+			if(activecar.getOrientation() == Orientation.HORIZONTAAL)
+				activecar.setPos(new PointF(activecar.getPos().x + 1, activecar.getPos().y));
+			else
+				activecar.setPos(new PointF(activecar.getPos().x, activecar.getPos().y + 1));
+		}
+		v.invalidate();
 		return false;
 	}
 
