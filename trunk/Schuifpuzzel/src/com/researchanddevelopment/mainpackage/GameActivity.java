80dc7eaa23a869025906f0781502f65daa204b34
thom@thomwiggers.nl <thom@thomwiggers.nl@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-23 13:57:37 +0000

    Level Select Erbij

--- trunk/Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameActivity.java
+++ trunk/Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameActivity.java
@@ -6,27 +6,30 @@ import android.os.Bundle;
 
 public class GameActivity extends Activity {
 	BoardView view;
-	
-	Board bord ;
-	
-	/** Called when the activity is first created. */
+
+	Board bord;
+
+	/**
+	 * Called when the activity is first created.
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		int res = getResources().getIdentifier("game01", "xml", this.getPackageName());
+
+		String resourcename = "game01";
+		if (this.getIntent().hasExtra("gameid"))
+			resourcename = String.format("game%02d", this.getIntent()
+					.getExtras().getInt("gameid"));
+
+		int res = getResources().getIdentifier(resourcename, "xml",
+				this.getPackageName());
 		bord = new Board(getResources().getXml(res));
 		view = new BoardView(this);
 		view.setBackgroundColor(Color.BLACK);
-		
+
 		bord.setBoardView(view);
-		
+
 		setContentView(view);
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
