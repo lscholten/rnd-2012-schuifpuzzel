6e03fa0ea386503be666745f7516533484581adc
CoolguyNL@gmail.com <CoolguyNL@gmail.com@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-23 10:33:53 +0000

    Splash

---
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/Splash.java
@@ -0,0 +1,36 @@
+package com.researchanddevelopment.mainpackage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	@Override
+	protected void onCreate(Bundle bundle) {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle);
+		setContentView(R.layout.splash);
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(4000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openStartingPoint = new Intent(
+							getApplicationContext(), SchuifpuzzelActivity.class);
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+}
