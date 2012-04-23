9d3e6cb61d7b6e08fb154ca7789734a48dc47ea8
CoolguyNL@gmail.com <CoolguyNL@gmail.com@e5a02a0d-b94b-1502-3536-973cca28ed34>
2012-04-23 15:34:55 +0000

    Lekker water

--- Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameActivity.java
+++ Schuifpuzzel/src/com/researchanddevelopment/mainpackage/GameActivity.java
@@ -3,9 +3,14 @@ package com.researchanddevelopment.mainpackage;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GameActivity extends Activity {
 	BoardView view;
+	private String resourcename;
+	private int gameid;
 
 	Board bord;
 
@@ -16,20 +21,51 @@ public class GameActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		String resourcename = "game01";
-		if (this.getIntent().hasExtra("gameid"))
+		resourcename = "game01";
+		gameid = 1;
+		if (this.getIntent().hasExtra("gameid")){
 			resourcename = String.format("game%02d", this.getIntent()
 					.getExtras().getInt("gameid"));
+			this.getIntent().getExtras().getInt("gameid");
+		}
+	
 
 		int res = getResources().getIdentifier(resourcename, "xml",
 				this.getPackageName());
-		bord = new Board(getResources().getXml(res));
+		bord = new Board(getResources().getXml(res), this, gameid);
 		view = new BoardView(this);
 		view.setBackgroundColor(Color.BLACK);
 
 		bord.setBoardView(view);
 
 		setContentView(view);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.game_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.Reset:
+				int res = getResources().getIdentifier(resourcename, "xml",
+						this.getPackageName());
+				bord = new Board(getResources().getXml(res), this, gameid);
+				view = new BoardView(this);
+				view.setBackgroundColor(Color.BLACK);
+
+				bord.setBoardView(view);
+
+				setContentView(view);
+				return true;
+			default:
+				return false;
+		}
 	}
 	
 
